0b6d7220d6bb5c7172bb6c04d9d60219cd15924b
MotokiNakae <motoki@cleverdog.me>
2022-08-23 15:22:33 +0900

    ソート時の赤枠線の挙動、特定セル選択について修正

--- SAMPLE_for_NC_2208/wwwroot/JavaScript.js
+++ SAMPLE_for_NC_2208/wwwroot/JavaScript.js
@@ -5,6 +5,11 @@
         args.cellInfo.borderTopWidth = 2;
         args.cellInfo.borderRightWidth = 2;
         args.cellInfo.borderLeftWidth = 2;
+    } else {
+        args.cellInfo.borderBottomWidth = 0;
+        args.cellInfo.borderTopWidth = 0;
+        args.cellInfo.borderRightWidth = 0;
+        args.cellInfo.borderLeftWidth = 0;
     }
 }
 igRegisterScript("onColumnDataBound", onColumnDataBound, false);
